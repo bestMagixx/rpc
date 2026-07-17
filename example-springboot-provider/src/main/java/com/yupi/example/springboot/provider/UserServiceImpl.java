3005c0a268053559f7bd63701edb7e777c708147
yk <clax@quickSee.com>
2026-07-17 19:48:31 +0800

    新增了启动机制和注解驱动

---
+++ example-springboot-provider/src/main/java/com/yupi/example/springboot/provider/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.yupi.example.springboot.provider;
+
+import com.yupi.example.common.model.User;
+import com.yupi.example.common.service.UserService;
+import com.yupi.yurpc.springboot.starter.annotation.RpcService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 用户实现类
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @learn <a href="https://codefather.cn">鱼皮的编程宝典</a>
+ * @from <a href="https://yupi.icu">编程导航学习圈</a>
+ */
+@Service
+@RpcService
+public class UserServiceImpl implements UserService {
+
+    public User getUser(User user){
+        System.out.println("用户名:" + user.getName());
+        return user;
+    }
+}
---
+++ yu-rpc-spring-boot-starter/src/main/java/com/yupi/yurpc/springboot/starter/bootstrap/RpcConsumerBootstrap.java
@@ -0,0 +1,52 @@
+package com.yupi.yurpc.springboot.starter.bootstrap;
+
+import com.yupi.yurpc.proxy.ServiceProxyFactory;
+import com.yupi.yurpc.springboot.starter.annotation.RpcReference;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+import java.lang.reflect.Field;
+
+/**
+ * Rpc服务提供者启动
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @learn <a href="https://codefather.cn">编程宝典</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public class RpcConsumerBootstrap implements BeanPostProcessor {
+
+    /**
+     * Bean 初始化后执行，注入服务
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throw BeansException
+     */
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName){
+        Class<?> beanClass = bean.getClass();
+        //遍历对象的所有属性
+        Field[] declaredFields = beanClass.getDeclaredFields();
+        for(Field field : declaredFields){
+            RpcReference rpcReference = field.getAnnotation(RpcReference.class);
+            if(rpcReference != null){
+                //为属性生成代理对象
+                Class<?> interfaceClass = rpcReference.interfaceClass();
+                if(interfaceClass == void.class){
+                    interfaceClass = field.getType();
+                }
+                field.setAccessible(true);
+                Object proxyObject = ServiceProxyFactory.getProxy(interfaceClass);
+                try{
+                    field.set(bean, proxyObject);
+                    field.setAccessible(false);
+                }catch (IllegalAccessException e){
+                    throw new RuntimeException("为字段注入代理对象失败", e);
+                }
+            }
+        }
+
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
